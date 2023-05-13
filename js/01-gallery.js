057c6cd7d692efc2d8df122d11caff0897fcc306
YuriyOhii <uaogiy@gmail.com>
2023-05-13 16:11:50 +0300

    randers gallery and connects library

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,30 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryRef = document.querySelector('.gallery');
+const galleryMarkup = createItemsMarkup(galleryItems);
 
-console.log(galleryItems);
+galleryRef.innerHTML = galleryMarkup;
+galleryRef.addEventListener('click', onUlGalleryClick);
+
+function onUlGalleryClick(e) {
+    e.preventDefault();
+    if (e.target.nodeName !== 'IMG') return;
+    console.log(e.target.dataset.source)
+}
+
+function createItemsMarkup(arr) {
+    return arr.map(el => 
+        `<li class="gallery__item">
+        <a class="gallery__link" href="${el.original}">
+          <img
+            class="gallery__image"
+            src="${el.preview}"
+            data-source="${el.original}"
+            alt="${el.description}"
+          />
+        </a>
+      </li>`
+    ).join('');
+}
+
+
